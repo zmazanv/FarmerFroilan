0c93a470248bbe3e1cd856e4120d40d4b2877651
Luciaw1219 <luciam1219@hotmail.com>
2023-05-02 19:50:58 -0400

    add interfaces and UML

---
+++ src/main/java/com/zipcodewilmington/froilansfarm/interfaces/Eater.java
@@ -0,0 +1,5 @@
+package com.zipcodewilmington.froilansfarm.interfaces;
+
+public interface Eater {
+    void eat(Edible edible);
+}
---
+++ src/main/java/com/zipcodewilmington/froilansfarm/interfaces/Edible.java
@@ -0,0 +1,4 @@
+package com.zipcodewilmington.froilansfarm.interfaces;
+
+public interface Edible {
+}
