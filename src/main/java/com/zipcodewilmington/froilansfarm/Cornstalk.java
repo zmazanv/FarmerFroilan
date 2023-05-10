d25b52fa06f41de853f56c206468d630f8250441
zmazanv <zmazanv@proton.me>
2023-05-10 12:08:34 -0400

    Correct Produce yield signature and create new classes

---
+++ src/main/java/com/zipcodewilmington/froilansfarm/Cornstalk.java
@@ -0,0 +1,8 @@
+package com.zipcodewilmington.froilansfarm;
+
+public class Cornstalk {
+    private EarCorn earCorn = new EarCorn();
+    public Cornstalk() {
+
+    }
+}
--- src/main/java/com/zipcodewilmington/froilansfarm/interfaces/Produce.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/interfaces/Produce.java
@@ -1,6 +1,6 @@
 package com.zipcodewilmington.froilansfarm.interfaces;
 
 public interface Produce {
-    void yield(Edible edible);
+    Edible yield();
 
 }
